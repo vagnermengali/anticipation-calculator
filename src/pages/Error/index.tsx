837b05431f5d2e75b6c8ce6fca8553aaada9be73
Vagner Mengali <101332741+vagnermengali@users.noreply.github.com>
2023-01-16 23:32:59 -0300

    feat: adding 404 route page with mobile

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,12 +1,24 @@
+import { Link } from "react-router-dom";
 import { StyledErrorSection } from "./style";
 
 const ErrorPage = () => {
-
+    document.title = "Page not found";
     return (
-        <StyledErrorSection>
-        </StyledErrorSection>
-    )
+            <StyledErrorSection>
+                <div className="container">
+                    <div className="content">
+                        <h1>Oops!</h1>
+                        <div className="div-links">
+                            <p>Algo deu errado...</p>
+                            <p>A página que você procurava não pode ser encontrada.</p>
+                            <Link className="link" to="/">
+                                Clique aqui para voltar para a página inicial
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </StyledErrorSection>
+    );
 }
 
-export default ErrorPage
-
+export default ErrorPage;
